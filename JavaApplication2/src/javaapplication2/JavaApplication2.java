e87a963b8e03da6820972abdf62bf644a2aad73b
RifaldyT_6706160067 <37770360+rifaldyt@users.noreply.github.com>
2018-03-28 11:13:41 +0700

    Add files via upload

---
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+import java.util.Scanner;
+
+public class JavaApplication2 {
+
+    public static void main(String[] args) {
+       
+  Scanner in = new Scanner (System.in); 
+     int bilangan;
+      System.out.print("Masukan Data : " );
+     bilangan = in.nextInt();
+     fibbonacci (bilangan);
+       
+    }
+    
+   public static void fibbonacci (int bilangan) {
+       if (bilangan == 0){
+           System.out.print("0");
+       } else if (bilangan == 1){
+           System.out.print("0 1");
+       }else { 
+           System.out.print("0 1 ");
+           int a = 0;
+           int b = 1;
+           for (int i = 1; i < bilangan; i++) {
+               int number = a + b;
+               System.out.print(number + " ");
+               a= b ;
+               b = number;
+               
+           }
+       }
+
+    }
+    
+}
